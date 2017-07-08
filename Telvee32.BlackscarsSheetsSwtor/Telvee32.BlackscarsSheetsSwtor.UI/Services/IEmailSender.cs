4f869538e915de4500f2c4a7f3d297e31760a5d9
Telvee32 <kjp32.telv@gmail.com>
2017-07-08 08:37:01 +0100

    Initial solution commit

---
+++ Telvee32.BlackscarsSheetsSwtor/Telvee32.BlackscarsSheetsSwtor.UI/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telvee32.BlackscarsSheetsSwtor.UI.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
